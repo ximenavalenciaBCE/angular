8bae9612863921eb8d529807a8174a5af448e712
xvalencia <xialvamo@gmail.com>
2025-07-03 18:16:07 -0500

    actualizacion primera version

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule} from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true
 })
 export class App {
-  protected title = 'TODO-LIST';
+  task ="";
+  tasks : {id:number, name:string}[] = [];
+
+  addTask(){
+    if (this.task.trim()==="") return;
+    this.tasks.push({id: this.tasks.length +1, name: this.task});
+    this.task ="";
+  }
+
+  removeTask(id: number){
+    this.tasks = this.tasks.filter( task => task.id !== id);
+  }
 }
